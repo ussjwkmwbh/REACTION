b8155feac5dce31f49e5a8a5bf5021d0b9d8d0fa
ussjwkmwbh <adityadey778@gmail.com>
2024-11-12 16:44:26 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,12 @@ function App() {
       <>
         <h1>Namaste paaji</h1>
         <Navbar />
-        <ProCard name="LadduBhai" about="This is a Metaverse Character" isPro={true} img_src="https://tr.rbxcdn.com/30DAY-Avatar-280E6D654E773B28FBD5033E38E0A475-Png/352/352/Avatar/Webp/noFilter"/>
-        <ProCard name="Adani" about="Chairman of Adani Group, Richest person in Asia" alt="Prime Minister" />
+        <h2>About Them:</h2>
+        <div>
+          <ProCard name="LadduBhai" about="This is a Metaverse Character" isPro={true} img_src="https://tr.rbxcdn.com/30DAY-Avatar-280E6D654E773B28FBD5033E38E0A475-Png/352/352/Avatar/Webp/noFilter"/>
+          <ProCard name="Adani" about="Chairman of Adani Group, Richest person in Asia" alt="Prime Minister" />
+          <ProCard name="Mark Zukerburg" about="Owner of Meta, React too"></ProCard>
+        </div>      
       </>
   )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,13 @@ function Footer(){
     const styles = {
         backgroundColor: "hsl(0, 0%, 60%)",
         color: "white",
+        textAlign: 'center'
     }
     return(
         <>
             <footer style={styles}>
                 <p>This Website is Made using ReactJs</p>
+                <p>2024. No Rights Reserved</p>
             </footer>
         </>
     )
