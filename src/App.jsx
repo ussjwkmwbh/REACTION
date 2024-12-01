c1e899c2d941b44f8151e5e58ea15aeb197f6ae6
ussjwkmwbh <adityadey778@gmail.com>
2024-12-01 21:24:50 +0530

    link

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <h2>About Them:</h2>
         <div>
           <ProCard />
-          <ProCard name="LadduBhai" about="This is a Metaverse Character" isPro={true}/>
+          <ProCard name="LadduBhai" about="This is a Metaverse Character" isPro={true} img_src="https://tr.rbxcdn.com/30DAY-Avatar-F9719D35712C7A2C018D8CF43891BFDC-Png/150/150/Avatar/Webp/noFilter"/>
           <ProCard id="adani" name="Gautam Adani" about="Chairman of Adani Group, Richest person in Asia" img_src={Adani} alt="Prime Minister" />
           <ProCard name="Mark Zukerburg" about="Owner of Meta, React too"></ProCard>
         </div>
